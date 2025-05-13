abce755413291460d2512f78b94aa61fee68c8a6
Joaquin Lopez <159632612+elgatohumilde@users.noreply.github.com>
2025-05-13 17:33:58 -0500

    feat: add Async annotation

--- src/main/java/com/dbp/recu_hack1/listener/EmailNotificationListener.java
+++ src/main/java/com/dbp/recu_hack1/listener/EmailNotificationListener.java
@@ -5,6 +5,7 @@ import com.dbp.recu_hack1.event.OrderCreatedEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,6 +14,7 @@ public class EmailNotificationListener {
     private static final Logger logger = LogManager.getLogger(EmailNotificationListener.class);
 
     @EventListener
+    @Async
     public void handleOrderCreatedEvent(OrderCreatedEvent event) {
         String email = event.getOrder().getEmail();
         logger.info("Email sent to {} confirming order {}", email, event.getOrder().getId());
