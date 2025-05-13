161550922a2e5ff5cf2b577ce86efab27e0add7e
Joaquin Lopez <159632612+elgatohumilde@users.noreply.github.com>
2025-05-13 16:41:11 -0500

    feat: insert project structure

---
+++ src/main/java/com/dbp/recu_hack1/RecuHack1Application.java
@@ -0,0 +1,13 @@
+package com.dbp.recu_hack1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecuHack1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecuHack1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbp/recu_hack1/RecuHack1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbp.recu_hack1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecuHack1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
