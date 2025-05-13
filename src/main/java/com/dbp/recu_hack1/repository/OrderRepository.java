cccd93a750f506d93f2ebd76149964d3db9bd683
Joaquin Lopez <159632612+elgatohumilde@users.noreply.github.com>
2025-05-13 17:56:30 -0500

    minor changes

--- src/main/java/com/dbp/recu_hack1/repository/OrderRepository.java
+++ src/main/java/com/dbp/recu_hack1/repository/OrderRepository.java
@@ -3,6 +3,8 @@ package com.dbp.recu_hack1.repository;
 import com.dbp.recu_hack1.domain.Order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 };
