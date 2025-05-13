d4d7b6955cc605e8c47c38b4e1b361b3e16f86ce
Joaquin Lopez <159632612+elgatohumilde@users.noreply.github.com>
2025-05-13 17:32:13 -0500

    feat: add controller

--- src/main/java/com/dbp/recu_hack1/controller/OrderController.java
+++ src/main/java/com/dbp/recu_hack1/controller/OrderController.java
@@ -1,5 +1,11 @@
 package com.dbp.recu_hack1.controller;
 
+import com.dbp.recu_hack1.domain.Order;
+import com.dbp.recu_hack1.event.OrderCreatedEvent;
+
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/order")
 public class OrderController {
 
+    private ApplicationEventPublisher eventPublisher;
+
+    @PostMapping
+    public String createOrder(@RequestBody Order order) {
+        eventPublisher.publishEvent(new OrderCreatedEvent(this, order));
+        return "New order: " + order.getId();
+    }
 
 }
